8dd25bb8497dc2b745a7cb9ace37c8b2e9e32f5f
Vanessacor <vanessapcbarros@gmail.com>
2020-02-25 18:08:46 +0100

    change sync and paralel to promises in seedDB

--- models/book.js
+++ models/book.js
@@ -1,28 +1,26 @@
 'use strict'
 
-'use strict'
+const mongoose = require('mongoose')
 
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const BookSchema = new Schema(
   {
-    title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
-    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
-    status:{type: String, required: true, enum: ['Read', 'Unread'], default: 'Unread'},
-    summary: {type: String, required: true},
-    isbn: {type: String, required: true},
+    title: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+    genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
+    status: { type: String, required: true, enum: ['Read', 'Unread'], default: 'Unread' },
+    summary: { type: String, required: true },
+    isbn: { type: String, required: true }
   }
-);
+)
 
 // Virtual for book's URL
 BookSchema
   .virtual('url')
   .get(function () {
-    return '/catalog/book/' + this._id;
-  });
+    return '/catalog/book/' + this._id
+  })
 
-//Export model
-module.exports = mongoose.model('Book', BookSchema);
+// Export model
+module.exports = mongoose.model('Book', BookSchema)
