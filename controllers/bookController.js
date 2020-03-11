b1d6400c1e9e544bd6e6ba9785e0b8a9eda1d079
Vanessacor <vanessapcbarros@gmail.com>
2020-03-11 16:45:46 +0100

    fix book form on post request and fix genre display on book detail

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,6 +3,8 @@ const Book = require('../models/book')
 const Author = require('../models/author')
 const Genre = require('../models/genre')
 
+const validator = require('express-validator')
+
 exports.index = function (req, res) {
   res.render('index')
 }
@@ -22,6 +24,28 @@ exports.bookList = function (req, res, next) {
     })
 }
 
+exports.bookDetail = function (req, res, next) {
+  Book.findById(req.params.id)
+    .populate('author')
+    .populate('genre')
+    .exec()
+    .then((book) => {
+      if (!book) { // No results.
+        var err = new Error('book not found')
+        err.status = 404
+        return next(err)
+      }
+      // Successful, so render.
+      const data = {
+        title: book.title,
+        book: book
+      }
+      res.render('book-detail', data)
+    })
+    .catch((err) => {
+      next(err)
+    })
+}
 // Display add book form on Get
 exports.addBookForm = function (req, res, next) {
   const authorQuery = Author.find()
@@ -47,6 +71,95 @@ exports.addBookForm = function (req, res, next) {
     })
 }
 
-exports.addBookPost = function (req, res) {
-  res.send('NOT IMPLEMENTED: add book form')
+exports.addBookPost = [
+
+  // Validate fields.
+  validator.check('title', 'title must not be empty.').isLength({ min: 1 }).trim(),
+  validator.check('author', 'Author must not be empty.').isLength({ min: 1 }).trim(),
+  validator.check('genre', 'Genre must not be empty.').isLength({ min: 1 }).trim(),
+  validator.check('status', 'Status must be selected.').isLength({ min: 1 }).trim(),
+  validator.check('summary', 'Summary must not be empty').isLength({ min: 1 }).trim(),
+  validator.check('isbn', 'isbn must not be empty').isLength({ min: 1 }).trim(),
+
+  // Sanitize fields (using wildcard).
+  validator.sanitizeBody('*').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validator.validationResult(req)
+
+    // Create a Book object with escaped and trimmed data.
+    const book = new Book(
+      {
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        status: req.body.status,
+        summary: req.body.summary,
+        isbn: req.body.isbn
+      })
+
+    if (!errors.isEmpty()) {
+      console.log('error', errors)
+      res.redirect('/catalog/book/add')
+    } else {
+      // Data from form is valid. Save product.
+      book.save(function (err) {
+        if (err) {
+          console.log('err', err)
+          return next(err)
+        }
+        // successful - redirect to new product record.
+        console.log('new book', book.url)
+        res.redirect(book.url)
+      })
+    }
+  }
+]
+
+// display delete book on Get
+exports.deleteBookGet = function (req, res, next) {
+  Book.findById(req.params.id)
+    .exec()
+
+    .then((results) => {
+      const books = results
+
+      if (books == null) { // No results.
+        res.redirect('/catalog/book')
+      }
+
+      const data = {
+        title: 'Delete Book',
+        book: books
+      }
+      res.render('book-delete', data)
+    })
+    .catch((error) => {
+      next(error)
+    })
+}
+
+exports.deleteBookPost = function (req, res, next) {
+  Book.findById(req.params.id)
+    .exec()
+
+    .then((results) => {
+      const book = results
+
+      if (book == null) { // No results.
+        res.redirect('/catalog/book')
+      } else {
+        Book.findByIdAndRemove(book, function deleteBook (err) {
+          if (err) {
+            return next(err)
+          }
+          res.redirect('/catalog/book/list')
+        })
+      }
+    })
+    .catch((error) => {
+      next(error)
+    })
 }
--- routes/catalog.js
+++ routes/catalog.js
@@ -10,13 +10,22 @@ const bookController = require('../controllers/bookController')
 // GET catalog home page.
 router.get('/', bookController.index)
 
-// GET Book List
-router.get('/book/list', bookController.bookList)
-
 // GET request for add a book
 router.get('/book/add', bookController.addBookForm)
 
 // POST request to add a book
 router.post('/book/add', bookController.addBookPost)
+
+// GET Book List
+router.get('/book/list', bookController.bookList)
+
+// GET request to specific book
+router.get('/book/:id', bookController.bookDetail)
+
+// GET request to delete book
+router.get('/book/:id/delete', bookController.deleteBookGet)
+
+// POST request to delete book
+router.post('/book/:id/delete', bookController.deleteBookPost)
 
 module.exports = router
